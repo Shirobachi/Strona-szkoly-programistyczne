763aa3c64c43443d6dd13bfa3004ba5b44d71c03
Simon Hryszko <simon@hryszko.dev>
2021-06-16 10:57:11 +0200

    Update default db connection variables

--- modules/.dbconnection.php
+++ modules/.dbconnection.php
@@ -1,8 +1,8 @@
 <?php
 
-  $_server = "front.mikr.us";
-  $_login = "f180";
-  $_pass = "NDM3_3eee";
-  $_db = "db_f180";
+  $_server = "localhost";
+  $_login = "";
+  $_pass = "";
+  $_db = "kurs";
 
 ?>
